8e3bc853f20ae9019b1ff034b27899e14f5e6c91
Hannah <hwhyatt@liverpool.ac.uk>
2025-09-30 09:38:46 +0100

    allow repeated resource seached for multiple terms

--- civic_data_server/tools/search_resources.py
+++ civic_data_server/tools/search_resources.py
@@ -39,29 +39,28 @@ def register(mcp):
     async def search_resources(
         query: Annotated[
             str,
-            Field(description="The query to search for resources. This must be a single keyword.")
+            Field(description="The query to search for resources.")
         ]
         ) -> str:
         """Searches directly for individual files (resources) like CSVs or PDFs using keywords. Use this as a shortcut when the user asks for a specific file, report, or data format. Returns the resource name, format, URL, resource ID, and the ID of the dataset it belongs to (package_id).
         """
-        # Ensure query string is a single keyword
-        if " " in query:
-            return "Error: Query must be a SINGLE keyword"
         
         ckan_api_base_url = "https://www.liverpoolcivicdata.com/api/3"
 
+        query_words = query.split(" ")
+    
+
         headers = {'Authorization': ckan_user_api_key}
-        path = f"{ckan_api_base_url}/action/resource_search?query=name:{query}"
 
-
-        try:
+        result_list = []
+        for word in query_words:
+            path = f"{ckan_api_base_url}/action/resource_search?query=name:{word}"
             response = requests.get(path, headers=headers, timeout=10)
             response.raise_for_status()
             data = response.json()
-
             if data.get("success") == True:
-                return format_resource_search_response(data)
+                result_list.append(format_resource_search_response(data))
             else:
-                return f"Error retrieving resource search results: {data.get('error')}"
-        except requests.RequestException as e:
-            return f"Error retrieving resource search results: {str(e)}"
+                continue
+
+        return "\n\n".join(result_list)
